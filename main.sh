68df359bc750936b178dbcd8745008230e49e316
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2025-05-21 01:29:39 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -17,17 +17,32 @@ dnf install -y \
 dnf remove -y \
   gnome-tour 
 
+# ATENÇAO: os comandos a seguir para "dark mode" e "shotcuts" estão sendo 
+#          feitos com dconf ao invés de gsetings, porque no primeiro login 
+#          ainda não temos uma sessão gráfica
+
 # dark mode
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-gsettings set org.gnome.desktop.background primary-color '#2c3e50'
-gsettings set org.gnome.desktop.interface accent-color 'purple'
+#gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+#gsettings set org.gnome.desktop.background primary-color '#2c3e50'
+#gsettings set org.gnome.desktop.interface accent-color 'purple'
+
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
+dconf write /org/gnome/desktop/background/primary-color "'#2c3e50'"
+dconf write /org/gnome/desktop/interface/accent-color "'purple'"
 
 # shortcuts
-gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+#gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+#gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+#gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+#gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "[]"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "[]"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift><Alt>Tab']"
+
 
 # set graphical as default
 systemctl set-default graphical.target 
