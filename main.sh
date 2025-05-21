827afbdc82e812a62d751653dffd3a606782b15e
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2025-05-21 00:25:54 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,29 @@
-echo "teste"
+### GNOME ###
+
+# install
+dnf install -y 
+gdm \
+gnome-shell \
+gnome-terminal \
+nautilus \
+nautilus-open-terminal \
+adobe-source-code-pro-fonts
+
+# cleanup
+gnome remove -y \
+gnome-tour 
+
+# dark mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.shell.settings-background color-scheme 'Yaru-dark'
+gsettings set org.gnome.desktop.background primary-color '#667c4d'
+
+# shortcuts
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+
+# set graphical as default
+systemctl set-default graphical.target 
