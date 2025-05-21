7782482ad618a81f0a1d5fc61dbd0307a0b0efce
Daniel de Andrade Varela <1902294+drachehavoc@users.noreply.github.com>
2025-05-21 01:50:30 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -27,21 +27,24 @@ dnf remove -y \
 #gsettings set org.gnome.desktop.background primary-color '#2c3e50'
 #gsettings set org.gnome.desktop.interface accent-color 'purple'
 
-dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
-dconf write /org/gnome/desktop/background/primary-color "'#2c3e50'"
-dconf write /org/gnome/desktop/interface/accent-color "'purple'"
-
 # shortcuts
 #gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 #gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
 #gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 #gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 
-dconf write /org/gnome/desktop/wm/keybindings/switch-applications "[]"
-dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "[]"
-dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift><Alt>Tab']"
+# dark mode
+dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'Adwaita-dark'"
+dconf write /org/gnome/desktop/background/primary-color "'#2c3e50'"
+dconf write /org/gnome/desktop/interface/accent-color "'purple'"
+
+# shortcuts
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "@as []"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "@as []"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "@as ['<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "@as ['<Shift><Alt>Tab']"
+
 
 
 # set graphical as default
